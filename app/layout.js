671ce37085c83cd321bfdac28008a8e8c20476f8
Abdul Rehman Khan <khan456.tdm@gmail.com>
2025-02-02 20:45:03 +0500

    [+]

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,8 @@ import { Geist, Geist_Mono, Poppins } from "next/font/google";
 import "./globals.css";
 
 const poppins = Poppins({
-  weight: ["100", "200", "300", "400", "700", "600", "800", "900"]
+  weight: ["100", "200", "300", "400", "700", "600", "800", "900"],
+  subsets: ["latin"]
 })
 
 export const metadata = {
